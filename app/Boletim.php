62b1ae39dd4abd920124977d17e7bb9c544b7205
Robson Fernando Duda <robsonferduda@gmail.com>
2022-06-27 22:36:49 -0300

    Dashboard

--- app/Boletim.php
+++ app/Boletim.php
@@ -12,4 +12,47 @@ class Boletim extends Model
     protected $table = 'app_boletins';
 
     protected $fillable = ['status_envio'];
+    
+    /*
+    SELECT 
+                            web.id as id, 
+                            web.id_cliente,
+                            clientes.nome, 
+                            area.titulo as area,
+                            web.titulo as titulo, 
+                            web.data_clipping as data, 
+                            '' as segundos,
+                            web.sinopse as sinopse, 
+                            web.uf as uf, 
+                            web.link as link, 
+                            web.status as status, 
+                            web.printurl as printurl, 
+                            cidade.titulo as cidade_titulo, 
+                            veiculo.titulo as INFO1,
+                            parte.titulo as INFO2, 
+                            ''  as INFOHORA,
+                            CONCAT('web','') as clipagem,
+                            area.ordem as ordem      
+                        FROM app_web as web 
+                        LEFT JOIN app_clientes as clientes ON web.id_cliente = clientes.id 
+                        LEFT JOIN app_web_sites as veiculo ON veiculo.id = web.id_site
+                        LEFT JOIN app_web_secao as parte ON parte.id = web.id_secao 
+                        LEFT JOIN app_cidades as cidade ON cidade.id = web.id_cidade 
+                        LEFT JOIN app_areasmodalidade as area ON (web.id_area = area.id)
+                        WHERE data_clipping = '2022-06-26'
+                        ORDER BY clientes.nome, titulo 
+                        
+SELECT 
+                            web.id_cliente,
+                            clientes.nome
+                        FROM app_web as web 
+                        LEFT JOIN app_clientes as clientes ON web.id_cliente = clientes.id 
+                        LEFT JOIN app_web_sites as veiculo ON veiculo.id = web.id_site
+                        LEFT JOIN app_web_secao as parte ON parte.id = web.id_secao 
+                        LEFT JOIN app_cidades as cidade ON cidade.id = web.id_cidade 
+                        LEFT JOIN app_areasmodalidade as area ON (web.id_area = area.id)
+                        WHERE data_clipping = '2022-06-26'
+                        GROUP BY web.id_cliente, clientes.nome 
+                        ORDER BY clientes.nome */
+                        
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,198 +1,83 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="nc-icon nc-briefcase-24 text-info"></i>
+    <div class="row">    
+        <div class="col-md-6">
+            <div class="card card-timeline card-plain">
+                <h6>{{ date('d/m/Y') }}</h6>
+                <div class="card-content">
+                  <ul class="timeline timeline-simple">
+                     <li class="timeline-inverted">
+                        <div class="timeline-badge danger">
+                           <i class="fa fa-comments fa-2x"></i>
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h6>Veículos Não Identificados <span class="badge badge-pill badge-danger pull-right">3 VEÍCULOS</span></h6>
+                            </div>
+                            <div class="timeline-body">
+                                <p></p>
                             </div>
                         </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Clientes</p>
-                                <p class="card-title"><a href="{{ url('clientes') }}">{{ $clientes }}</a></p>
+                     </li>
+                     <li class="timeline-inverted">
+                        <div class="timeline-badge success">
+                           <i class="fa fa-tags fa-2x mt-1"></i>
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h6>IDENTIFICAR ÁREAS <span class="badge badge-pill badge-success pull-right">13 NOTÍCIAS</span></h6>
+                            </div>
+                            <div class="timeline-body">
+                                <p>Existem xx notícias sem identificação das áreas.</p>
+                                <p>Todos os clientes possuem áreas cadastradas.</p>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="card-footer ">
-                        <hr>
-                        <div class="stats">
-                            <a href="{{ url('clientes/create') }}"><i class="fa fa-plus text-info"></i> Novo</a>                
+                     </li>
+                     <li class="timeline-inverted">
+                        <div class="timeline-badge info">
+                           <i class="fa fa-cogs font-20 mt-1"></i>
                         </div>
-                </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h6>GERAR BOLETINS <span class="badge badge-pill badge-primary pull-right">23 CLIENTES</span></h6>
+                            </div>
+                            <div class="timeline-body">
+                                <table id="bootstrap-table" class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>CÓDIGO</th>
+                                            <th>NOME</th>
+                                            <th class="center">VER</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr data-index="0">
+                                            <td></td>
+                                            <td>7</td>
+                                            <td class="center">
+                                                <a href="{{ url('noticias/cliente/4') }}"><i class="fa fa-eye"></i></a>
+                                            </td>
+                                        </tr>                                      
+                                    </tbody>
+                                 </table>
+                                <hr/>
+                                <button type="button" class="btn btn-sm btn-primary pull-right">
+                                    <span class="btn-label"><i class="fa fa-check"></i></span> Gerar Boletins
+                                </button>
+                            </div>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
             </div>
-        </div>  
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="fa fa-facebook text-facebook"></i>
-                            </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Média Facebook</p>
-                                <p class="card-title">{{ $media_facebook }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer ">
-                    <hr>
-                    <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
-                </div>
-            </div>
-        </div>  
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="fa fa-instagram text-pink""></i>
-                            </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Média Instagram</p>
-                                <p class="card-title">{{ $media_instagram }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer ">
-                    <hr>
-                    <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
-                </div>
-            </div>
-        </div>    
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="fa fa-twitter text-info"></i>
-                            </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Média de Tweets</p>
-                                <p class="card-title"><a>{{ $media_twitter }}</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer ">
-                    <hr>
-                    <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
-                </div>
-            </div>
-        </div>   
+         </div>
     </div> 
-    <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12">
-                            <div class="custom-cloud" id='cloud'></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> 
-
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-12 col-md-12">
-                            <h6 class="mt-2 mb-3">
-                                Termos Ativos
-                                <a class="pull-right" href="{{ url('terms/client', session('cliente')['id']) }}"><i class="fa fa-plus"></i> Adicionar</a>
-                            </h6>
-                            @foreach($terms as $term)
-                                <p>{{ $term->term }}</p>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>   
-  
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-12 col-md-12">
-                            <h6 class="mt-2 mb-3">
-                                Hashtags Ativas
-                                <a class="pull-right" href="{{ url('client/hashtags', session('cliente')['id']) }}"><i class="fa fa-plus"></i> Adicionar</a>
-                            </h6>
-                            @foreach($hashtags as $hashtag)
-                                <p>#{{ $hashtag->hashtag }}</p>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>   
-    </div>
 @endsection
 @section('script')
 <script>
     $(document).ready(function() {
 
-        $('body').loader('show');
-
-        var APP_URL = {!! json_encode(url('/')) !!};
-        var tamanho = 0.02;
-
-        fetch(APP_URL+'/nuvem-palavras/words', {
-            method: 'GET', 
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-        }).then(function(response) {
-            return response.json();
-        }).then(function(response){
-            
-            let words = [];
-
-            $('body').loader('hide');
-            const _token = $('meta[name="csrf-token"]').attr('content');
-
-            Object.entries(response).forEach(element => {
-                words.push(
-                    {
-                        text: element[0], 
-                        weight: element[1],
-                        html: {
-                            class: 'cloud-word'
-                        },                        
-                    }
-                );
-            });
-
-            let cloud = $('#cloud').jQCloud(words, {
-                autoResize: true,
-                classPattern: null,
-                colors: ["#66C2A5", "#FC8D62", "#800026", "#E78AC3", "#A6D854", "#FFD92F", "#E5C494", "#B3B3B3"],
-                fontSize: function (width, height, step) {
-                    if (step < 5)
-                        tamanho = tamanho - 0.001;
-                    return width * tamanho * step + 'px';
-                }
-            });            
-        });
+        
     });
 </script>
 @endsection
