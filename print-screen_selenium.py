f23fc999858d4c0252470639f704ff30926f8275
Robson <robson@coperve.ufsc.br>
2022-12-20 15:50:25 -0300

    Print Screen

--- print-screen_selenium.py
+++ print-screen_selenium.py
@@ -1,11 +1,31 @@
 from selenium import webdriver
+from PIL import Image
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-options = Options()
-options.headless = True
+url = 'https://www.globo.com/'
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-driver.get('https://www.urlbox.io')
-driver.save_screenshot('public/screen/screenshot.png')
-driver.quit()
+#run first time to get scrollHeight
+driver = webdriver.Chrome()
+driver.get(url)
+#pause 3 second to let page load
+time.sleep(3)
+#get scroll Height
+height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight )")
+print(height)
+#close browser
+driver.close()
+
+#Open another headless browser with height extracted above
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument(f"--window-size=1920,{height}")
+chrome_options.add_argument("--hide-scrollbars")
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(url)
+#pause 3 second to let page loads
+time.sleep(3)
+#save screenshot
+driver.save_screenshot('public/screen/shot.png')
+driver.close()
