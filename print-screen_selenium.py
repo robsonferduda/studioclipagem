34ed8bd8befc516448557eb41c699cf62ac15805
Robson <robson@coperve.ufsc.br>
2022-12-22 14:48:16 -0300

    Screen

--- print-screen_selenium.py
+++ print-screen_selenium.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from PIL import Image
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
 
-url = 'https://www.globo.com/'
+url = 'https://www.nsctotal.com.br/noticias/sc-segue-com-alerta-para-deslizamentos-apesar-de-tregua-na-chuva'
 
 #run first time to get scrollHeight
 driver = webdriver.Chrome()
@@ -23,11 +24,16 @@ chrome_options.add_argument(f"--window-size=1920,{height}")
 chrome_options.add_argument("--hide-scrollbars")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_experimental_option("prefs", { "profile.default_content_setting_values.geolocation": 1})
 driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
-
 driver.get(url)
+
 #pause 3 second to let page loads
 time.sleep(3)
 #save screenshot
+
+driver.find_element("xpath", "//button[text()='Não, obrigado']").click()
+driver.find_element(By.CLASS_NAME, "tp-close").click()
+
 driver.save_screenshot('public/screen/shot.png')
 driver.close()
