44a94f3a642e114ec390b9747b9fc5eeb9e95bac
Robson Fernando Duda <robsonferduda@gmail.com>
2023-01-13 20:27:57 -0300

    Monitoramento

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,10 @@ Route::match(array('GET', 'POST'),'buscar-impresso','JornalImpressoController@in
 
 Route::get('jornal-web','JornalWebController@index');
 
+Route::get('monitoramento','MonitoramentoController@index');
+Route::get('monitoramento/adicionar','MonitoramentoController@adicionar');
+Route::get('monitoramento/{id}/desabilitar','MonitoramentoController@desabilitar');
+
 Route::get('radios','RadioController@index');
 Route::match(array('GET', 'POST'),'radio/emissoras','RadioController@emissoras');
 Route::get('radio/emissora/{id}/transcricao/atualiza','RadioController@atualizaTranscricao');
