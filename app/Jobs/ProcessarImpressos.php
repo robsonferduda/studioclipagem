9ecfe2376a1b75d0a34a964d632823f67de9ffe3
Robson <robson@coperve.ufsc.br>
2023-01-18 11:56:03 -0300

    Processamento de Arquivos

--- app/Jobs/ProcessarImpressos.php
+++ app/Jobs/ProcessarImpressos.php
@@ -16,21 +16,11 @@ class ProcessarImpressos implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
         $data['dados'] = null;
