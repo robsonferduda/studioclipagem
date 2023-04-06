7fa9a0ac9a66bdfee31255bed2864710b91334f0
Robson <robson@coperve.ufsc.br>
2023-04-06 15:28:54 -0300

    Dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -175,11 +175,12 @@
                                             @endforeach                                    
                                         </tbody>
                                     </table>
+                                    <a href="{{ url('monitoramento/listar') }}">Ver Mais</a>
                                 @else
                                     <p><i class="fa fa-hourglass-start mr-1"></i>Nenhum monitoramento realizado no dia de hoje</p>
                                 @endif
                             </div>
-                            <a href="{{ url('monitoramento/listar') }}">Ver Mais</a>
+                            
                         </div>
                     </li>
                   </ul>
