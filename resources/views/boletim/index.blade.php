f2ba84e08f7630c5a92a46b1094421a57d2349fa
Robson <robson@coperve.ufsc.br>
2023-11-29 17:42:40 -0300

    Boletim

--- resources/views/boletim/index.blade.php
+++ resources/views/boletim/index.blade.php
@@ -5,10 +5,14 @@
         <div class="card-header">
             <div class="row">
                 <div class="col-md-6">
-                    <h4 class="card-title ml-2"><i class="fa fa-file-o"></i> Boletins</h4>
+                    <h4 class="card-title ml-2">
+                        <i class="fa fa-file-o"></i> Boletins
+                        <i class="fa fa-angle-double-right" aria-hidden="true"></i> Cadastrados 
+                        <i class="fa fa-angle-double-right" aria-hidden="true"></i> {{ \Carbon\Carbon::parse(Session::get('data_atual'))->format('d/m/Y') }}
+                    </h4>
                 </div>
                 <div class="col-md-6">
-                    
+                    <button class="btn btn-primary pull-right mr-3"><i class="fa fa-plus"></i> Novo</button>
                 </div>
             </div>
         </div>
@@ -16,46 +20,25 @@
             <div class="col-md-12">
                 @include('layouts.mensagens')
             </div>
-            <div class="col-md-12">
-                <table class="table table-hover">
-                    <thead class="">
-                        <tr>
-                            <th class="center">Código</th>
-                            <th class="center">Data da Criação</th>
-                            <th>Cliente</th>
-                            <th>Boletim</th>
-                            <th class="text-center">Status</th>
-                            <th class="text-center">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($boletins as $boletim)
-                            <tr>
-                                <td class="center">{{ $boletim->id }}</td>
-                                <td class="center">{{ Carbon\Carbon::parse($boletim->data)->format('d/m/Y H:i') }}</td>
-                                <td>
-                                    @if($boletim->id_cliente == 452)
-                                        Offshore
-                                    @else
-                                        Zurich Airport
-                                    @endif
-                                </td>
-                                <td>{{ $boletim->titulo }}</td>
-                                <td class="text-center">
-                                    @if($boletim->status_envio == 'enviado')
-                                        <span class="badge badge-success">Enviado</span>
-                                    @else
-                                        <span class="badge badge-danger">Não enviado</span>
-                                    @endif
-                                </td>
-                                <td class="text-center">
-                                    <a href="{{ url('boletim', $boletim->id) }}"><i class="fa fa-eye"></i></a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>   
-            </div>        
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group m-3 w-70">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label class="font-black"><i class="fa fa-filter"></i> Filtrar por cliente</label>
+                                    <select class="form-control select2" name="regra" id="regra">
+                                        <option value="">Selecione um cliente</option>
+                                        @foreach ($clientes as $cliente)
+                                            <option value="{{ $cliente->id }}">{{ $cliente->pessoa->nome }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>     
+                    </div>
+                </div>   
+            </div>     
         </div>
     </div>
 </div> 
