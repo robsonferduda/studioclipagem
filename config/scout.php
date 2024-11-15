5e0d4978216b363984e58a73db64586af262350b
Robson Fernando Duda <robsonferduda@gmail.com>
2024-11-14 21:15:38 -0300

    Busca

--- config/scout.php
+++ config/scout.php
@@ -113,7 +113,7 @@ return [
 
     'algolia' => [
         'id' => env('ALGOLIA_APP_ID', ''),
-        'secret' => env('ALGOLIA_SECRET', ''),
+        'secret' => env('ALGOLIA_API_KEY', ''),
     ],
 
 ];
