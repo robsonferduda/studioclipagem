707ea8197a01639a6c4483c43f0637fecd0fccde
Robson Fernando Duda <robsonferduda@gmail.com>
2024-11-25 22:38:19 -0300

    Web

--- app/Http/Controllers/MonitoramentoController.php
+++ app/Http/Controllers/MonitoramentoController.php
@@ -85,9 +85,9 @@ class MonitoramentoController extends Controller
 
     public function getConteudo($id)
     {
-        $sql = "SELECT ts_headline('portuguese','', to_tsquery('portuguese', 'Copacabana'), 'HighlightAll=true, StartSel=<mark>, StopSel=</mark>')";
+        $sql = "SELECT ts_headline('portuguese','Eu visitei a praia de Copacabana e achei muito bonita.', to_tsquery('portuguese', 'Copacabana'), 'HighlightAll=true, StartSel=<mark>, StopSel=</mark>')";
         $dados = DB::select($sql);
-        
+
         return response()->json($dados); 
     }
 
