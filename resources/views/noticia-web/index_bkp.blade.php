f670a74cca7e698a6d7e0b00258234593b8b5286
Robson Fernando Duda <robsonferduda@gmail.com>
2024-12-26 12:36:50 -0300

    Fontes - Listar

--- resources/views/noticia-web/index_bkp.blade.php
+++ resources/views/noticia-web/index_bkp.blade.php
@@ -3,10 +3,10 @@
 <div class="col-md-12">
     <div class="card">
         <div class="card-header">
-            <div class="row ml-1">
+            <div class="row">
                 <div class="col-md-6">
                     <h4 class="card-title">
-                        <i class="fa fa-globe"></i> Web 
+                        <i class="fa fa-globe"></i> Jornal Web 
                         <i class="fa fa-angle-double-right" aria-hidden="true"></i> Notícias 
                     </h4>
                 </div>
@@ -63,8 +63,58 @@
                         </div>
                     {!! Form::close() !!} 
 
-                
-                    
+                    @if($dados->count())
+                        <h6 class="px-3">Mostrando {{ $dados->count() }} de {{ $dados->total() }} Notícias</h6>
+                    @endif
+
+                    {{ $dados->onEachSide(1)->appends(['dt_inicial' => $dt_inicial, 'dt_final' => $dt_final])->links('vendor.pagination.bootstrap-4') }}    
+
+                    @foreach ($dados as $key => $noticia)
+                        <div class="card">
+                            <div class="card-body">                           
+                                <div class="row">
+                                    <div class="col-lg-12 col-sm-12 mb-1">
+                                        <p><strong>{{ $noticia->titulo_noticia }}</strong></p>
+                                        <div>
+                                            @if( \Carbon\Carbon::parse($noticia->data_noticia)->format('d/m/Y') == '01/01/1999')
+                                                <span class="badge badge-pill badge-warning"> {{ \Carbon\Carbon::parse($noticia->data_insert)->format('d/m/Y') }}</span>
+                                            @else
+                                                <span class="badge badge-pill badge-default"> {{ \Carbon\Carbon::parse($noticia->data_noticia)->format('d/m/Y') }}</span>
+                                            @endif
+                                            {{ ($noticia->fonte) ? $noticia->fonte->nome : 'Fonte desconhecida' }} - Coletada em {{ \Carbon\Carbon::parse($noticia->data_insert)->format('d/m/Y H:i:s') }} 
+                                        </div>
+                                        
+                                        <div class="panel panel-success">
+                                            <div class="conteudo-noticia">
+                                                {!! ($noticia->conteudo) ?  Str::limit($noticia->conteudo->conteudo, 300, " ...")  : '<span class="text-danger">Nenhum conteúdo coletado</span>' !!}
+                                            </div>
+                                            <div class="panel-body">
+                                                {!! ($noticia->conteudo) ? $noticia->conteudo->conteudo : '<span class="text-danger">Nenhum conteúdo coletado</span>' !!}
+                                            </div>
+                                            <div class="panel-heading">
+                                                <h3 class="panel-title"><span class="btn-show">Mostrar Mais</span></h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-10">
+                                        @if($noticia->categoria)
+                                            <span class="badge badge-pill badge-primary">{{ $noticia->categoria }}</span>
+                                        @else
+                                            <span class="badge badge-pill badge-default">Sem Categoria</span>
+                                        @endif
+                                    </div>
+                                    <div class="col-lg-4 col-sm-2">
+                                        <a class="btn btn-success btn-sm pull-right" href="{{ asset('noticia/web/detalhes/'.$noticia->id) }}" role="button"><i class="fa fa-eye"> </i> Detalhes</a>
+                                        <a class="btn btn-warning btn-sm pull-right" href="{{ $noticia->url_noticia }}" target="_BLANK" role="button"><i class="fa fa-globe"> </i> Ver Original</a>
+                                        <a class="btn btn-info btn-sm pull-right" href="{{ asset('noticia/web/estatisticas/'.$noticia->id) }}" role="button"><i class="nc-icon nc-chart-bar-32"> </i> Estatísticas</a>
+                                    </div>
+                                </div>                               
+                            </div>
+                            
+                        </div>
+                    @endforeach
+
+                    {{ $dados->onEachSide(1)->appends(['dt_inicial' => $dt_inicial, 'dt_final' => $dt_final])->links('vendor.pagination.bootstrap-4') }} 
                 </div>
             </div>
         </div>
