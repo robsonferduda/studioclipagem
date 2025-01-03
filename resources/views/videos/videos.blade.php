d9a638ec328e154e267ff650642cad4b64d55780
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-03 11:13:54 -0300

    Programas

--- resources/views/videos/videos.blade.php
+++ resources/views/videos/videos.blade.php
@@ -84,13 +84,12 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-lg-4 col-sm-12">     
-                                        {{--                                       
+                                    <div class="col-lg-4 col-sm-12">                                            
                                         <video width="100%" height="240" controls>
                                             <source src="{{ (Storage::disk('s3')) ? Storage::disk('s3')->temporaryUrl($video->video_path, '+30 minutes') : '' }}" type="video/mp4">
                                             <source src="movie.ogg" type="video/ogg">
                                             Seu navegador não suporta a exibição de vídeos.
-                                        </video> --}}
+                                        </video>
                                     </div>
                                     <div class="col-lg-8 col-sm-12">
                                         <p class="mb-1"><strong>{{ ($video->programa and $video->programa->emissora) ? $video->programa->emissora->nome_emissora : '' }}</strong> - <strong>{{ ($video->programa) ? $video->programa->nome_programa : '' }}</strong></p>
