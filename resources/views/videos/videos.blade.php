3e8d7e2d90823dae06dd4be0b73d63737d44c151
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-03 17:16:08 -0300

    Atualizações

--- resources/views/videos/videos.blade.php
+++ resources/views/videos/videos.blade.php
@@ -94,7 +94,7 @@
                                     <div class="col-lg-8 col-sm-12">
                                         
                                         <p class="mb-1">
-                                            @if($video->emissora and $video->emissora->tipo and in_array($video->emissora->tipo->id, [4,5]))
+                                            @if($video->programa and $video->programa->tipo and in_array($video->programa->tipo->id, [4,5]))
                                                 <i class="fa fa-youtube text-danger" aria-hidden="true" style="font-size: 30px;"></i>
                                             @endif
                                             <strong>{{ ($video->programa and $video->programa->emissora) ? $video->programa->emissora->nome_emissora : '' }}</strong> - 
