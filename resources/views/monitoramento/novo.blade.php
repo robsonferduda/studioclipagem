1d715ae4c21ed36d0b57826faa112498cccd5d85
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-06 15:57:16 -0300

    Atualização

--- resources/views/monitoramento/novo.blade.php
+++ resources/views/monitoramento/novo.blade.php
@@ -253,10 +253,11 @@
 
                                 $("#accordion_web").append('<div class="card card-plain">'+
                                   '<div class="card-header" role="tab" id="heading1">'+
+                                    '<strong>'+v.nome+'</strong>'+
                                     '<a data-toggle="collapse" data-parent="#accordion_web" href="#collapse_'+v.id+'" data-tipo="web" data-chave="card-web-txt-'+k+'" data-id="'+v.id+'" aria-expanded="false" aria-controls="collapseOne" class="collapsed fts_detalhes"> '+data_formatada+' - '+v.titulo_noticia+
                                       '<i class="nc-icon nc-minimal-down"></i>'+
                                     '</a>'+
-                                    '<strong>'+v.nome+'</strong>'+
+                                    
                                   '</div>'+
                                   '<div id="collapse_'+v.id+'" class="collapse" role="tabpanel" aria-labelledby="heading1" style="">'+
                                     '<div class="box-destaque-busca destaque-card-web-txt-'+k+'"></div><div class="card-body card-busca card-web-txt-'+k+'">'+
