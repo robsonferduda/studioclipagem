7a8dcc581e427983b738bce3e21b56fa788bedbc
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-08 21:15:16 -0300

    Atualizações

--- app/Http/Controllers/JornalImpressoController.php
+++ app/Http/Controllers/JornalImpressoController.php
@@ -366,7 +366,7 @@ class JornalImpressoController extends Controller
     {
         Session::put('sub-menu','upload');
 
-        $jornais_pendentes = EdicaoJornalImpresso::where('fl_upload', true)->orderBy('fl_processado','DESC')->get();
+        $jornais_pendentes = EdicaoJornalImpresso::where('fl_upload', true)->orderBy('fl_processado','ASC')->get();
 
         return view('jornal-impresso/upload', compact('jornais_pendentes'));
     }
