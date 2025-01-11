f728339ce366efbd24400ada2ad9a6a0bbf9cb84
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-11 19:47:46 -0300

    Cron

--- app/Console/Commands/WebCron.php
+++ app/Console/Commands/WebCron.php
@@ -22,10 +22,11 @@ class WebCron extends Command
 
         app('App\Http\Controllers\MonitoramentoController')->executarWeb();
         
+        /*
         Mail::send('notificacoes.teste', $data, function($message) use ($titulo){
             $message->to("robsonferduda@gmail.com")
                     ->subject($titulo);
             $message->from('boletins@clipagens.com.br','Studio Clipagem');
-        }); 
+        }); */
     }
 }
