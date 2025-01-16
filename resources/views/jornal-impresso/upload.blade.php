26c788b1519e3b5d325b42e3e05aa6d934a33d5e
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-16 20:55:37 -0300

    Jornais Impressos

--- resources/views/jornal-impresso/upload.blade.php
+++ resources/views/jornal-impresso/upload.blade.php
@@ -74,7 +74,7 @@
                                     @if(count($jornal->paginas))
                                         <div class="pull-right">
                                             <span class="badge badge-pill badge-success">Processado</span>
-                                            {{ count($jornal->paginas) }} páginas
+                                            <p>{{ count($jornal->paginas) }} páginas</p>
                                         </div>
                                     @else
                                         <div class="pull-right">
