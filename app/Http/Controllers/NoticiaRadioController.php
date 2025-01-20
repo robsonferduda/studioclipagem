b2c059a06e50e29fb9528ef5d6f8ddfae94a15a1
Robson Fernando Duda <robsonferduda@gmail.com>
2025-01-20 19:27:51 -0300

    Dados

--- app/Http/Controllers/NoticiaRadioController.php
+++ app/Http/Controllers/NoticiaRadioController.php
@@ -81,7 +81,7 @@ class NoticiaRadioController extends Controller
                         return $q->whereIn('emissora_radio.id', $fonte);
                     })
                     ->when($dt_inicial, function ($q) use ($dt_inicial, $dt_final) {
-                        return $q->whereBetween('gravacao_emissora_radio.created_at', [$dt_inicial, $dt_final]);
+                        return $q->whereBetween('gravacao_emissora_radio.created_at', [$dt_inicial." 00:00:00", $dt_final." 23:59:59"]);
                     })
                     ->paginate(10);
 
