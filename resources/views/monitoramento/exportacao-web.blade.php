8f0da43d792c29b2237508581ca6c0d2ebd5ff33
Robson Fernando Duda <robsonferduda@gmail.com>
2025-02-03 09:43:37 -0300

    Atualizações

--- resources/views/monitoramento/exportacao-web.blade.php
+++ resources/views/monitoramento/exportacao-web.blade.php
@@ -66,10 +66,10 @@
                                 <div class="card">
                                     <div class="card-body">
                                         <p class="mb-1"><a href="{{ $monitoramento->url_noticia }}" target="BLANK">{{ $monitoramento->titulo_noticia }}</a></p>
-                                        <p class="mb-1"><strong>{{ $monitoramento->nome_cliente }} - {{ $monitoramento->nome_fonte }}</strong></p>
+                                        <p class="mb-1 text-muted"><strong>ID {{ $monitoramento->noticia_id }} - {{ $monitoramento->nome_cliente }} - {{ $monitoramento->nome_fonte }}</strong></p>
                                         <code>{{ $monitoramento->expressao }}</code>
                                         <p class="mb-1" style="font-family: DejaVu Sans Mono, monospace;">
-                                            <i class="fa fa-clock-o fa-1x"></i> Coletado em {{ \Carbon\Carbon::parse($monitoramento->created_at)->format('d/m/Y H:i:s') }} 
+                                            <i class="fa fa-clock-o fa-1x"></i> Notícia de {{ \Carbon\Carbon::parse($monitoramento->data_noticia)->format('d/m/Y H:i:s') }} coletada em {{ \Carbon\Carbon::parse($monitoramento->created_at)->format('d/m/Y H:i:s') }} 
                                         </p>  
                                         <div class="pull-right">
                                             @if($monitoramento->exported)
