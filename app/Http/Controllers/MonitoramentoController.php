9ff06150e9f2fb79845902e7f59374d3ea6f70f5
Robson Fernando Duda <robsonferduda@gmail.com>
2025-02-13 13:44:56 -0300

    Atualização

--- app/Http/Controllers/MonitoramentoController.php
+++ app/Http/Controllers/MonitoramentoController.php
@@ -148,11 +148,12 @@ class MonitoramentoController extends Controller
                     })
                     ->join('fonte_web','fonte_web.id','=','noticias_web.id_fonte')
                     ->join('monitoramento', 'monitoramento.id','=','noticia_cliente.monitoramento_id')
+                    /*
                     ->when($dt_inicial, function ($q) use ($dt_inicial, $dt_final) {
                         return $q->whereBetween('noticia_cliente.created_at', [$dt_inicial." 00:00:00", $dt_final." 23:59:59"]);
-                    })
+                    })*/
                     ->when($fl_dia, function ($q) use ($fl_dia, $dt_final) {
-                        return $q->whereBetween('noticias_web.data_noticia', [$dt_final." 00:00:00", $dt_final." 23:59:59"]);
+                        return $q->whereBetween('noticias_web.data_noticia', [$dt_inicial." 00:00:00", $dt_final." 23:59:59"]);
                     })
                     ->when($exportacao, function ($q) use ($exportacao) {
                         if($exportacao == 1){
