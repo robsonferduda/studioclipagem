b3867545e20b0de1e4b39214ba6ca1b1da6604fa
Robson Fernando Duda <robsonferduda@gmail.com>
2025-04-08 22:24:53 -0300

    Fontes

--- app/Http/Controllers/JornalImpressoController.php
+++ app/Http/Controllers/JornalImpressoController.php
@@ -179,6 +179,10 @@ class JornalImpressoController extends Controller
             $jornais->whereBetween('created_at', [$dt_inicial." 00:00:00", $dt_final." 23:59:59"]);
 
             $jornais->when($fonte_selecionada, function ($q) use ($fonte_selecionada) {
+
+
+                dd("Entrou");
+
                 $q->whereHas('edicao', function($q) use($fonte_selecionada){
                     return $q->whereIn('id_jornal_online', $fonte_selecionada);
                 });
