ef0ba975b3d8b1c6412deee16f6057cfa1cb6dbf
Robson <robson@coperve.ufsc.br>
2025-04-10 16:21:08 -0300

    Buscar Impresso

--- resources/views/jornal-impresso/index.blade.php
+++ resources/views/jornal-impresso/index.blade.php
@@ -138,8 +138,8 @@
                                         </div>                
                                     </div>
                                     <div class="col-lg-12 col-md-12 col-sm-12 mb-1"> 
-                                        <button class="btn btn-primary btn-visualizar-noticia" data-id="{{ $pagina->id }}"><i class="fa fa-eye"></i> Visualizar</button> 
-                                        <a href="{{ url('jornal-impresso/noticia/extrair/web',$pagina->id) }}" class="btn btn-success btn-extrair-noticia"><i class="fa fa-database"></i> Extrair Notícia</a>  
+                                        <button class="btn btn-primary btn-visualizar-noticia" data-id="{{ $pagina->id_pagina }}"><i class="fa fa-eye"></i> Visualizar</button> 
+                                        <a href="{{ url('jornal-impresso/noticia/extrair/web',$pagina->id_pagina) }}" class="btn btn-success btn-extrair-noticia"><i class="fa fa-database"></i> Extrair Notícia</a>  
                                     </div>
                                 </div>     
                             </div>
