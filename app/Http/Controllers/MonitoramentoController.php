b14d1e6250e6396e07e5e9bc50c48821cabc4876
Robson <robson@coperve.ufsc.br>
2025-05-14 12:08:53 -0300

    Bug TV

--- app/Http/Controllers/MonitoramentoController.php
+++ app/Http/Controllers/MonitoramentoController.php
@@ -677,7 +677,6 @@ class MonitoramentoController extends Controller
                         programa_emissora_web pew 
                             ON pew.id = n.id_programa_emissora_web
                         WHERE 1=1
-                            AND n.created_at >= now() - interval '24' hour
                             AND n.created_at BETWEEN '$dt_inicial' AND '$dt_final' ";
 
                 if($monitoramento->filtro_tv){
@@ -859,8 +858,7 @@ class MonitoramentoController extends Controller
                         JOIN 
                         programa_emissora_web pew 
                         ON pew.id = n.id_programa_emissora_web
-                        WHERE 1=1
-                        AND n.created_at >= now() - interval '24' hour "; 
+                        WHERE 1=1 "; 
 
                 if($monitoramento->filtro_tv){
                     $sql .= "AND n.id_programa_emissora_web IN($monitoramento->filtro_tv)";
