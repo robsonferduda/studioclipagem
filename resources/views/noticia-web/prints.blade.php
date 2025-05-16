6f314eab0110170bd36e6d1e4eecdc4692ffbe1e
Robson Fernando Duda <robsonferduda@gmail.com>
2025-05-16 19:47:17 -0300

    Web

--- resources/views/noticia-web/prints.blade.php
+++ resources/views/noticia-web/prints.blade.php
@@ -86,7 +86,7 @@
                                             </p> 
                                             <p>Notícia cadastrada em {{ \Carbon\Carbon::parse($noticia->created_at)->format('d/m/Y H:i:s') }}</p>
                                         </div>
-                                        <a title="Notícia" href="{{ $noticia->url_noticia }}" class="btn btn-success btn-link btn-icon pull-right"><i class="fa fa-globe fa-2x"></i></a>
+                                        <a title="Notícia" href="{{ $noticia->url_noticia }}" target="_BLANK" class="btn btn-success btn-link btn-icon pull-right"><i class="fa fa-globe fa-2x"></i></a>
                                         <a title="Visualizar" href="{{ url('noticia/web/'.$noticia->id.'/ver') }}" class="btn btn-warning btn-link btn-icon pull-right"><i class="fa fa-link fa-2x"></i></a>
                                         <a title="Editar" href="{{ url('noticia/web/'.$noticia->id.'/editar') }}" class="btn btn-primary btn-link btn-icon pull-right"><i class="fa fa-edit fa-2x"></i></a>
                                     </div>
