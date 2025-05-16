8812c00bffce93e2dd35a4d41c23fedb84681c5f
Robson Fernando Duda <robsonferduda@gmail.com>
2025-05-16 19:36:13 -0300

    Web

--- resources/views/noticia-web/prints.blade.php
+++ resources/views/noticia-web/prints.blade.php
@@ -49,7 +49,7 @@
                     <p class="text-info">São listadas somente notícias que foram vinculadas a algum cliente via monitoramento</p>
                     
                     <h6>RESUMO</h6>
-                    <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                    <table class="table table-striped table-bordered" cellspacing="0" width="100%">
                         <thead>
                             <tr>
                                 <th>Fonte</th>
