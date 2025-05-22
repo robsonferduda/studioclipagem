01f887f554a2ed676dc8ac11c7ba490931f8dd35
Robson <robson@coperve.ufsc.br>
2025-05-22 10:58:00 -0300

    Programas

--- resources/views/noticia-web/monitoramento.blade.php
+++ resources/views/noticia-web/monitoramento.blade.php
@@ -135,7 +135,7 @@
                                                 <p class="font-weight-bold mb-1">{{ $dado->titulo_noticia }}</p>
                                                 <h6><a href="{{ url('fonte-web/editar', $dado->id_fonte) }}" target="_BLANK">{{ ($dado->nome_fonte) ? $dado->nome_fonte : '' }}</a></h6>  
                                                 <h6 style="color: #FF5722;">{{ ($dado->nm_estado) ? $dado->nm_estado : '' }}{{ ($dado->nm_cidade) ? "/".$dado->nm_cidade : '' }}</h6> 
-                                                <p class="text-muted"> {!! ($dado->data_noticia) ? date('d/m/Y', strtotime($dado->data_noticia)) : date('d/m/Y', strtotime($dado->data_noticia)) !!} - {{ $dado->nome_fonte }}</p> 
+                                                <p class="text-muted mb-1"> {!! ($dado->data_noticia) ? date('d/m/Y', strtotime($dado->data_noticia)) : date('d/m/Y', strtotime($dado->data_noticia)) !!} - {{ $dado->nome_fonte }}</p> 
                                                 <p class="mb-1"><i class="nc-icon nc-briefcase-24"></i> {{ ($dado->nome_cliente) ? $dado->nome_cliente : '' }}</p>
                                                 <div style="margin-bottom: 5px;" class="tags destaque-{{ $dado->noticia_id }}-{{ $dado->monitoramento_id }}" data-monitoramento="{{ $dado->monitoramento_id }}" data-chave="{{ $dado->noticia_id }}-{{ $dado->monitoramento_id }}" data-noticia="{{ $dado->noticia_id }}">
                                                 
