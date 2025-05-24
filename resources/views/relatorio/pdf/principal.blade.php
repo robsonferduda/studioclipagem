24bfa4905e0989de816cc76809c1991ce8a24c89
Robson Fernando Duda <robsonferduda@gmail.com>
2025-05-24 18:08:40 -0300

    dfdsf

--- resources/views/relatorio/pdf/principal.blade.php
+++ resources/views/relatorio/pdf/principal.blade.php
@@ -58,7 +58,7 @@
                             </p> 
                         </div>
                         <div style="text-align: center;">
-                            <img style="margin: 0 auto;" src="{{ public_path('img/clientes/logo/79.png') }}">
+                            <img style="margin: 0 auto;" src="{{ public_path('img/noticia-impressa/10235.jpg') }}">
                         </div>
             @if($key < count($dados_impresso) -1)
                 <div style="page-break-before: always;"></div>                        
