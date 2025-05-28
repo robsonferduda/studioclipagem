7fc9e8089bf638697ae6609983e281b3397b0791
Robson Fernando Duda <robsonferduda@gmail.com>
2025-05-28 20:54:50 -0300

    Atualizações

--- resources/views/noticia-web/detalhes.blade.php
+++ resources/views/noticia-web/detalhes.blade.php
@@ -25,10 +25,24 @@
                 <div class="col-lg-12 col-md-12 m-3">
                     <p><strong>{{ $noticia->titulo_noticia }}</strong></p>
                     <p>{{ ($noticia->fonte) ? $noticia->fonte->nome : 'Não identificada' }} - {{ \Carbon\Carbon::parse($noticia->data_noticia)->format('d/m/Y') }} - Coletada em {{ \Carbon\Carbon::parse($noticia->data_insert)->format('d/m/Y H:i:s') }}</p>
+                    <p><a class="text-info" href="{{ $noticia->url_noticia }}" target="_BLANK"><span><i class="fa fa-globe"></i> Ver Original</span></a></p>
                     <p>
                         {!! $noticia->conteudo->conteudo !!}
                     </p>
-                    <p><a href="{{ $noticia->url_noticia }}" target="_BLANK">Ver Original</a></p>
+                    
+                    @if($noticia->ds_caminho_img)
+                        <img src="{{ asset('img/noticia-web/'.$noticia->ds_caminho_img) }}" alt="Página {{ $noticia->ds_caminho_img }}">
+                    @elseif($noticia->path_screenshot)                                         
+                        <img src="{{ Storage::disk('s3')->temporaryUrl($noticia->path_screenshot, '+30 minutes') }}" 
+                                                alt="Print notícia {{ $noticia->noticia_id }}" 
+                                                class="img-fluid img-thumbnail" 
+                                                style="width: 100%; height: auto; border: none;">
+                    @else
+                        <img src="{{ asset('img/no-print.png') }}" 
+                                                alt="Sem Print" 
+                                                class="img-fluid img-thumbnail" 
+                                                style="width: 100%; height: auto; border: none;">
+                    @endif
                 </div>   
             </div>
         </div>
