c714c41782a9f9457a265291b289325c03b9b11c
Robson <robson@coperve.ufsc.br>
2025-05-29 15:27:45 -0300

    Editar

--- resources/views/noticia-tv/index.blade.php
+++ resources/views/noticia-tv/index.blade.php
@@ -86,7 +86,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 mb-1">                                   
+                                    <div class="col-lg-4 col-md-4 col-sm-12 mb-1">                                   
                                         @if($noticia->ds_caminho_video)
                                             <video width="100%" height="240" controls>
                                                 <source src="{{ asset('video/noticia-tv/'.$noticia->ds_caminho_video) }}" type="video/mp4">
@@ -99,7 +99,7 @@
                                     </div>
                                     <div class="col-lg-10 col-sm-10 mb-1"> 
                                         <div class="row">
-                                            <div class="col-lg-12 col-md-12 col-sm-12 mb-1"> 
+                                            <div class="col-lg-8 col-md-8 col-sm-12 mb-1"> 
                                                 <div class="conteudo-{{ $noticia->id }}">
                                                     
                                                     <h6><a href="{{ url('fonte-impresso/'.$noticia->id_fonte.'/editar') }}" target="_BLANK">{{ ($noticia->fonte) ? $noticia->fonte->nome_emissora : '' }}</a></h6>  
