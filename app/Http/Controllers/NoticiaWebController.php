8030f3d24cefbcd2495c42ff0ffdb036336cc125
Robson Fernando Duda <robsonferduda@gmail.com>
2025-05-29 19:34:07 -0300

    Atualizações

--- app/Http/Controllers/NoticiaWebController.php
+++ app/Http/Controllers/NoticiaWebController.php
@@ -499,7 +499,6 @@ class NoticiaWebController extends Controller
         $resumo = DB::select($sql);
 
         $erros = NoticiaWeb::where('path_screenshot','ilike','ERROR')
-                            ->orWhereNull('path_screenshot')
                             ->whereBetween('created_at', [$dt_inicial, $dt_final])
                             ->when($cliente_selecionado, function ($q) use ($cliente_selecionado) {
                                 return $q->whereHas('clientes', function($query) use ($cliente_selecionado) {
@@ -507,7 +506,7 @@ class NoticiaWebController extends Controller
                                 });
                             })
                             ->orderBy('id_fonte')
-                            ->get();
+                            ->toSql();
 
         return view('noticia-web/prints',compact('resumo','erros','dt_inicial','dt_final','clientes','cliente_selecionado'));
     }
