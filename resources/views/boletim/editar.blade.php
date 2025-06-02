df71283c53e7bea8f2eb9260dc612c4ad1fc3897
Robson <robson@coperve.ufsc.br>
2025-06-02 09:49:20 -0300

    BUG

--- resources/views/boletim/editar.blade.php
+++ resources/views/boletim/editar.blade.php
@@ -190,8 +190,6 @@
                 dt_final = $("#dt_final").val();
                 termo = $("#termo").val();
 
-                alert(boletim + ' - ' + cliente + ' - ' + flag_web + ' - ' + flag_impresso + ' - ' + flag_tv + ' - ' + flag_radio + ' - ' + dt_inicial + ' - ' + dt_final);
-
                 //Limpas os dados
                 dados = [];
                 
