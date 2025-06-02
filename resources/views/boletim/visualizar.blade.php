56897478918cc0c1d79a68c7a284bcb848b64f78
Robson Fernando Duda <robsonferduda@gmail.com>
2025-06-01 22:08:34 -0300

    dasd

--- resources/views/boletim/visualizar.blade.php
+++ resources/views/boletim/visualizar.blade.php
@@ -62,7 +62,7 @@
                         @endforeach
 
                         @if(count($noticias_tv) > 0)
-                            <p style="text-transform: uppercase; font-weight: 600;"><i class="fa fa-globe"></i> Clipagens de TV</p>
+                            <p style="text-transform: uppercase; font-weight: 600;"><i class="fa fa-tv"></i> Clipagens de TV</p>
                         @endif
                         @foreach($noticias_tv as $key => $noticia)
                             <div style="border-bottom: 1px solid #e3e3e3; margin-bottom: 10px; padding-bottom: 10px;">
@@ -75,6 +75,20 @@
                             </div>
                         @endforeach
 
+
+                        @if(count($noticias_radio) > 0)
+                            <p style="text-transform: uppercase; font-weight: 600;"><i class="fa fa-volume-up"></i> Clipagens de TV</p>
+                        @endif
+                        @foreach($noticias_radio as $key => $noticia)
+                            <div style="border-bottom: 1px solid #e3e3e3; margin-bottom: 10px; padding-bottom: 10px;">
+                                <p style="margin-bottom: 0px;"><strong>Título:</strong> {!! ($noticia->titulo) ? : '<span class="text-danger">Notícia sem título</span>' !!}</p>
+                                <p style="margin-bottom: 0px;"><strong>Data:</strong> {{ date('d/m/Y', strtotime($noticia->dt_noticia)) }}</p>
+                                <p style="margin-bottom: 0px;"><strong>Veículo:</strong> {{ $noticia->emissora->nome_emissora }}</p>
+                                <p style="margin-bottom: 0px;"><strong>Seção:</strong> {{ ($noticia->secao) ? $noticia->secao->ds_sessao : 'Não informado' }}</p>
+                                <p style="margin-bottom: 0px;"><strong>Sinopse:</strong> {!! $sinopse = strip_tags(str_replace('Sinopse 1 - ', '', $noticia->sinopse)) !!}</p>
+                                <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('img/noticia-web/'.$noticia->ds_caminho_img) }}" target="_blank">Veja</a></p>
+                            </div>
+                        @endforeach
 
                     </div>        
                 </div>
