21c015809a5e57d5c5afee51b826cc533ed18e65
Robson Fernando Duda <robsonferduda@gmail.com>
2025-06-03 08:27:28 -0300

    Boletim

--- resources/views/boletim/detalhes.blade.php
+++ resources/views/boletim/detalhes.blade.php
@@ -70,12 +70,12 @@
                     @endif
                     @foreach($noticias_tv as $key => $noticia)
                         <div style="border-bottom: 1px solid #e3e3e3; margin-bottom: 10px; padding-bottom: 10px;">
-                            <p style="margin-bottom: 0px;"><strong>Título:</strong> {!! ($noticia->titulo) ? : '<span class="text-danger">Notícia sem título</span>' !!}</p>
                             <p style="margin-bottom: 0px;"><strong>Data:</strong> {{ date('d/m/Y', strtotime($noticia->dt_noticia)) }}</p>
-                            <p style="margin-bottom: 0px;"><strong>Veículo:</strong> {{ $noticia->emissora->nome_emissora }}</p>
-                            <p style="margin-bottom: 0px;"><strong>Seção:</strong> {{ ($noticia->secao) ? $noticia->secao->ds_sessao : 'Não informado' }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Emissora:</strong> {{ $noticia->emissora->nome_emissora }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Programa:</strong> {{ ($noticia->programa) ? $noticia->programa->nome_programa : 'Não informado' }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Duração:</strong> {{ ($noticia->duracao) ? $noticia->duracao : 'Não informado' }}</p>
                             <p style="margin-bottom: 0px;"><strong>Sinopse:</strong> {!! $sinopse = strip_tags(str_replace('Sinopse 1 - ', '', $noticia->sinopse)) !!}</p>
-                            <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('img/noticia-web/'.$noticia->ds_caminho_img) }}" target="_blank">Veja</a></p>
+                            <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('video/noticia-tv/'.$noticia->ds_caminho_video) }}" target="_blank">Assista</a></p>
                         </div>
                     @endforeach
 
@@ -84,13 +84,13 @@
                     @endif
                     @foreach($noticias_radio as $key => $noticia)
                         <div style="border-bottom: 1px solid #e3e3e3; margin-bottom: 10px; padding-bottom: 10px;">
-                                <p style="margin-bottom: 0px;"><strong>Data:</strong> {{ date('d/m/Y', strtotime($noticia->dt_clipagem)) }}</p>
-                                <p style="margin-bottom: 0px;"><strong>Emissora:</strong> {{ $noticia->emissora->nome_emissora }}</p>
-                                <p style="margin-bottom: 0px;"><strong>Programa:</strong> {{ ($noticia->programa) ? $noticia->programa->nome_programa : 'Não informado' }}</p>
-                                <p style="margin-bottom: 0px;"><strong>Duração:</strong> {{ ($noticia->duracao) ? $noticia->duracao : 'Não informado' }}</p>
-                                <p style="margin-bottom: 0px;"><strong>Sinopse:</strong> {!! $sinopse = strip_tags(str_replace('Sinopse 1 - ', '', $noticia->sinopse)) !!}</p>
-                                <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('audio/noticia-radio/'.$noticia->ds_caminho_audio) }}" target="_blank">Ouça</a></p>
-                            </div>
+                            <p style="margin-bottom: 0px;"><strong>Data:</strong> {{ date('d/m/Y', strtotime($noticia->dt_clipagem)) }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Emissora:</strong> {{ $noticia->emissora->nome_emissora }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Programa:</strong> {{ ($noticia->programa) ? $noticia->programa->nome_programa : 'Não informado' }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Duração:</strong> {{ ($noticia->duracao) ? $noticia->duracao : 'Não informado' }}</p>
+                            <p style="margin-bottom: 0px;"><strong>Sinopse:</strong> {!! $sinopse = strip_tags(str_replace('Sinopse 1 - ', '', $noticia->sinopse)) !!}</p>
+                            <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('audio/noticia-radio/'.$noticia->ds_caminho_audio) }}" target="_blank">Ouça</a></p>
+                        </div>
                     @endforeach
                 </div>      
             </div>                  
--- resources/views/boletim/visualizar.blade.php
+++ resources/views/boletim/visualizar.blade.php
@@ -57,7 +57,7 @@
                                 <p style="margin-bottom: 0px;"><strong>Veículo:</strong> {{ $noticia->fonte->nome }}</p>
                                 <p style="margin-bottom: 0px;"><strong>Seção:</strong> {{ ($noticia->secao) ? $noticia->secao->ds_sessao : 'Não informado' }}</p>
                                 <p style="margin-bottom: 0px;"><strong>Sinopse:</strong> {!! $sinopse = strip_tags(str_replace('Sinopse 1 - ', '', $noticia->sinopse)) !!}</p>
-                                <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('img/noticia-web/'.$noticia->ds_caminho_img) }}" target="_blank">Veja</a></p>
+                                <p style="margin-bottom: 10px;"><strong>Print:</strong> <a href="{{ asset('img/noticia-web/'.$noticia->ds_caminho_img) }}" target="_blank">Veja</a></p>
                             </div>
                         @endforeach
 
@@ -66,12 +66,12 @@
                         @endif
                         @foreach($noticias_tv as $key => $noticia)
                             <div style="border-bottom: 1px solid #e3e3e3; margin-bottom: 10px; padding-bottom: 10px;">
-                                <p style="margin-bottom: 0px;"><strong>Título:</strong> {!! ($noticia->titulo) ? : '<span class="text-danger">Notícia sem título</span>' !!}</p>
                                 <p style="margin-bottom: 0px;"><strong>Data:</strong> {{ date('d/m/Y', strtotime($noticia->dt_noticia)) }}</p>
-                                <p style="margin-bottom: 0px;"><strong>Veículo:</strong> {{ $noticia->emissora->nome_emissora }}</p>
-                                <p style="margin-bottom: 0px;"><strong>Seção:</strong> {{ ($noticia->secao) ? $noticia->secao->ds_sessao : 'Não informado' }}</p>
+                                <p style="margin-bottom: 0px;"><strong>Emissora:</strong> {{ $noticia->emissora->nome_emissora }}</p>
+                                <p style="margin-bottom: 0px;"><strong>Programa:</strong> {{ ($noticia->programa) ? $noticia->programa->nome_programa : 'Não informado' }}</p>
+                                <p style="margin-bottom: 0px;"><strong>Duração:</strong> {{ ($noticia->duracao) ? $noticia->duracao : 'Não informado' }}</p>
                                 <p style="margin-bottom: 0px;"><strong>Sinopse:</strong> {!! $sinopse = strip_tags(str_replace('Sinopse 1 - ', '', $noticia->sinopse)) !!}</p>
-                                <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('img/noticia-web/'.$noticia->ds_caminho_img) }}" target="_blank">Veja</a></p>
+                                <p style="margin-bottom: 10px;"><strong>Link:</strong> <a href="{{ asset('video/noticia-tv/'.$noticia->ds_caminho_video) }}" target="_blank">Assista</a></p>
                             </div>
                         @endforeach
 
