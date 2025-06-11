95ee110f2d199e0d76f11693232ca8736e554d77
Robson Fernando Duda <robsonferduda@gmail.com>
2025-06-10 22:24:45 -0300

    Arquivo Vìdeo

--- resources/views/relatorio/pdf/principal.blade.php
+++ resources/views/relatorio/pdf/principal.blade.php
@@ -62,7 +62,7 @@
         @foreach($dados_impresso as $key => $noticia)
             
                         <div class="header">
-                            <h5 style="text-align: justify; margin-bottom: 0px; padding-bottom: 5px; margin-top: 26px; font-size: 17px; border-bottom: 1px solid black; color: red;">Clipagem de Jornal</h5>    
+                            <h5 style="text-align: justify; margin-bottom: 0px; padding-bottom: 5px; margin-top: 26px; font-size: 17px; border-bottom: 1px solid black; ">Clipagem de Jornal</h5>    
                             <p style="text-align: justify; font-size: 16px; margin:0px; padding: 0px; margin-top: 8px; margin-bottom: 8px;">
                                 {{ $noticia->fonte }}
                                 {{ ($noticia->secao) ? " - ".$noticia->secao  : '' }}
@@ -85,17 +85,17 @@
         <!-- Nome da Fonte - Seção - Página - Data - Cidade/UF -->
         @foreach($dados_web as $key => $noticia)
             <div class="header">
-                <h5 style="text-align: justify; margin-bottom: 0px; padding-bottom: 5px; margin-top: 26px; font-size: 17px; border-bottom: 1px solid black; color: red;">Clipagem de Web</h5>    
-                    <p style="text-align: justify; font-size: 16px; margin:0px; padding: 0px; margin-top: 8px; margin-bottom: 8px;">
+                <h5 style="text-align: justify; margin-bottom: 0px; padding-bottom: 5px; margin-top: 26px; font-size: 17px; border-bottom: 1px solid black; ">Clipagem de Web</h5>    
+                    <p style="text-align: justify; font-size: 16px; margin:0px; padding: 0px; margin-top: 8px; margin-bottom: 8px; position: relative;">
                         {{ $noticia->fonte }}
                         {{ " - ".$noticia->data_formatada }}
                         {{ ($noticia->nm_cidade) ? " - ".trim($noticia->nm_cidade."/".$noticia->sg_estado) : '' }}
                         {{ ($noticia->nm_estado and !$noticia->nm_cidade) ? " - ".trim($noticia->sg_estado) : ''}}
-                        <img style="width: 20px; height: 20px;" src="https://studioclipagem.com/img/globe.png">
+                        <img style="width: 15px; height: 15px; position: absolute; right: 0;" src="https://studioclipagem.com/img/globe.png">
                     </p> 
             </div>
             <div style="text-align: center;">
-                <img style="margin: 0 auto;" src="https://studioclipagem.com/img/noticia-impressa/{{ $noticia->midia }}">
+                <img style="margin: 0 auto;" src="https://studioclipagem.com/img/noticia-web/{{ $noticia->midia }}">
             </div>
             @if($key < count($dados_web) -1)
                 <div style="page-break-before: always;"></div>                        
