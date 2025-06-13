39aa0d582711230316db67344c7440ebcd3e9e27
Robson Fernando Duda <robsonferduda@gmail.com>
2025-06-13 09:57:33 -0300

    Listagem de Notícias

--- app/User.php
+++ app/User.php
@@ -55,6 +55,6 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class,'role_user','user_id','id');
+        return $this->belongsToMany(Role::class,'role_user','id','user_id');
     }
 }
