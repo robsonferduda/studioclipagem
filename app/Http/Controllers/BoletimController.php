1912d27c881c148a169530b1892125dfff1c8f0f
Robson <robson@coperve.ufsc.br>
2025-07-07 11:41:44 -0300

    Ordem Email

--- app/Http/Controllers/BoletimController.php
+++ app/Http/Controllers/BoletimController.php
@@ -736,7 +736,7 @@ class BoletimController extends Controller
 
         usort($noticias, function ($a, $b) {
             // Ordena por area
-            $areaCompare = strcmp($a['area'] ?? '', $b['area'] ?? '');
+            $areaCompare = strcmp($a['ordem'] ?? '', $b['ordem'] ?? '');
             if ($areaCompare !== 0) {
                 return $areaCompare;
             }
