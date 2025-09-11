b422fad35f5cd3af228545026ca4ae9577f608e5
Robson Fernando Duda <robsonferduda@gmail.com>
2025-09-10 22:47:40 -0300

    Emails

--- app/Http/Controllers/MonitoramentoController.php
+++ app/Http/Controllers/MonitoramentoController.php
@@ -677,7 +677,7 @@ class MonitoramentoController extends Controller
                             post_facebook post
                         WHERE
                             post.data_postagem BETWEEN '$dt_inicial' AND '$dt_final'
-                        AND post.tsv_mensagem @@ to_tsquery('simple', '$monitoramento->expressao')
+                        AND post.tsv_mensagem @@ to_tsquery('portuguese', '$monitoramento->expressao')
                         ORDER BY post.data_postagem DESC";
 
                 $dados = DB::select($sql);
@@ -744,7 +744,7 @@ class MonitoramentoController extends Controller
                             post_instagram post
                         WHERE
                             post.timestamp BETWEEN '$dt_inicial' AND '$dt_final'
-                        AND post.tsv_caption @@ to_tsquery('simple', '$monitoramento->expressao')
+                        AND post.tsv_caption @@ to_tsquery('portuguese', '$monitoramento->expressao')
                         ORDER BY post.timestamp DESC";
 
                 $dados = DB::select($sql);
