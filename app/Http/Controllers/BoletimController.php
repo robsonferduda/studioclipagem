0cec42984cec778cb3ad0f6e178ddd881a93e588
Robson <robson@coperve.ufsc.br>
2025-09-16 17:00:43 -0300

    Midia Boletim

--- app/Http/Controllers/BoletimController.php
+++ app/Http/Controllers/BoletimController.php
@@ -510,23 +510,17 @@ class BoletimController extends Controller
 
             $erro = '';
 
+            /*
             if($noticia_web->ds_caminho_img){
                 $path = 'img/noticia-web/'.$noticia_web->ds_caminho_img;
             }else{
 
                 $path = 'noticia/web/print/'.$noticia_web->id;
 
-                /*
-                try {
-                    $path = Storage::disk('s3')->temporaryUrl($noticia_web->path_screenshot, '+30 minutes');
-                } catch (Exception $e) {
-                    $path = '';
-                    $erro = 'Erro na imagem';
-                } catch (\Throwable $e){
-                    $path = '';
-                    $erro = 'Erro na imagem';
-                }*/
-            }
+               
+            }*/
+
+            $path = Storage::disk('s3')->get($noticia_web->path_screenshot);
 
             $area = (NoticiaCliente::where('noticia_id', $noticia_web->id)->where('cliente_id',$boletim->id_cliente)->where('tipo_id', 2)->first() and NoticiaCliente::where('noticia_id', $noticia_web->id)->where('cliente_id',$boletim->id_cliente)->where('tipo_id', 2)->first()->area) ?
                     Area::where('id', NoticiaCliente::where('noticia_id', $noticia_web->id)->where('cliente_id',$boletim->id_cliente)->where('tipo_id', 2)->first()->area)->first()->descricao :
