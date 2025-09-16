c24f50a61c75d4fa2ff32fa1c11a11e72d0355c1
Robson Fernando Duda <robsonferduda@gmail.com>
2025-09-16 19:19:00 -0300

    Boletim

--- app/Http/Controllers/BoletimController.php
+++ app/Http/Controllers/BoletimController.php
@@ -164,6 +164,7 @@ class BoletimController extends Controller
                 JOIN noticia_cliente t3 ON t3.noticia_id = t1.id
                 LEFT JOIN boletim_noticia t4 ON t4.id_noticia = t3.noticia_id AND id_tipo = 1 AND t4.id_boletim = $request->id_boletim
                 WHERE 1=1
+                AND retorno_midia NOTNULL
                 AND t1.deleted_at IS NULL 
                 AND t3.deleted_at IS NULL ";
 
@@ -205,6 +206,7 @@ class BoletimController extends Controller
                 LEFT JOIN boletim_noticia t4 ON t4.id_noticia = t3.noticia_id AND id_tipo = 3 AND t4.id_boletim = $request->id_boletim
                 LEFT JOIN programa_emissora_radio t5 ON t5.id = t1.programa_id
                 WHERE 1=1
+                AND duracao NOTNULL
                 AND t1.deleted_at IS NULL 
                 AND t3.deleted_at IS NULL ";
 
@@ -246,6 +248,7 @@ class BoletimController extends Controller
                 LEFT JOIN boletim_noticia t4 ON t4.id_noticia = t3.noticia_id AND id_tipo = 4 AND t4.id_boletim = $request->id_boletim
                 LEFT JOIN programa_emissora_web t5 ON t5.id = t1.programa_id
                 WHERE 1=1
+                AND duracao NOTNULL
                 AND t1.deleted_at IS NULL 
                 AND t3.deleted_at IS NULL ";
 
