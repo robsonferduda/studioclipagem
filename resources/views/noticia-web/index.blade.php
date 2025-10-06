4393df80af6baa15f48ebeea09f77424417c76f1
Robson <robson@coperve.ufsc.br>
2025-10-06 15:08:28 -0300

    Notícia Web

--- resources/views/noticia-web/index.blade.php
+++ resources/views/noticia-web/index.blade.php
@@ -207,12 +207,12 @@
                                                             <a href="javascript:void(0);" class="text-primary ver-mais" data-id="{{ $dado->id }}">[ver mais]</a>
                                                         @endif
                                                     </span>
-                                                    <span class="transcricao-completa d-none" id="transcricao-completa-{{ $dado->id }} conteudo-{{ $dado->id }}-{{ $dado->monitoramento->id }}">
+                                                    <span class="transcricao-completa d-none" id="transcricao-completa-{{ $dado->id }}">
                                                         {!! $dado->conteudo->conteudo !!}
                                                         <a href="javascript:void(0);" class="text-primary ver-menos" data-id="{{ $dado->id }}">[ver menos]</a>
                                                     </span>
                                                 </div>
-                                                <div class="panel-body conteudo-{{ $dado->id }}">
+                                                <div class="panel-body conteudo-{{ $dado->id }} conteudo-{{ $dado->id }}-{{ $dado->monitoramento->id }}">
                                                     {!! ($dado->conteudo) ?  $dado->conteudo->conteudo  : '<span class="text-danger">Nenhum conteúdo coletado</span>' !!}
                                                 </div>                                            
                                             </div>                                               
